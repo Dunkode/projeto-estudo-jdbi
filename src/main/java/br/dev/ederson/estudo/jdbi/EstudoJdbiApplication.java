c094d1bb1671b6afb1768dc1848cf0a0296cc668
Ederson <edersonvidal137@gmail.com>
2025-09-15 21:05:17 -0300

    Configurações iniciais do projeto

---
+++ src/main/java/br/dev/ederson/estudo/jdbi/EstudoJdbiApplication.java
@@ -0,0 +1,13 @@
+package br.dev.ederson.estudo.jdbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoJdbiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstudoJdbiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/dev/ederson/estudo/jdbi/EstudoJdbiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.ederson.estudo.jdbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoJdbiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
