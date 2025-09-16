1ee40ea80ed927c9d47e7b75d236995f3112dd95
Ederson <edersonvidal137@gmail.com>
2025-09-15 21:12:25 -0300

    Configurações iniciais do projeto

---
+++ src/main/resources/scripts/createClientes.sql
@@ -0,0 +1,10 @@
+CREATE TABLE clientes
+(
+    id              UUID PRIMARY KEY,
+    nome            VARCHAR(200) NOT NULL,
+    email           VARCHAR(200) NOT NULL UNIQUE,
+    telefone        VARCHAR(30),
+    data_nascimento DATE,
+    ativo           BOOLEAN      NOT NULL DEFAULT TRUE,
+    data_criacao    TIMESTAMP             DEFAULT now()
+);
